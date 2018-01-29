dedac0a9c2bfc76475efb4ef663d37a687bad9f3
Elad Alfassa <elad@fedoraproject.org>
2018-01-29 22:24:02 +0200

    Port to pydbus
    (dbus-python is deprecated and obsolete)

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ led-proxy exposes a simplistic dbus service that allows user to control the scro
 
 Usage
 -----
-To install, run `sudo ./setup.sh`
+Install dependencies: `sudo dnf intall python3-pydbus python3-gobject`
 
-Then start it, `sudo systemctl enable led-proxy.service --now`
-and then you can talk to `com.eladalfassa.LedProxy` on the DBus system bus.
+Run `sudo ./setup.sh` to install the service.
+
+Start the service: `sudo systemctl enable led-proxy.service --now`
+and now you can talk to `com.eladalfassa.LedProxy` on the DBus system bus.
 
 If you want to use it the same way you would've used `xset` in your shell scripts,
 use `dbus-send`. For example:
@@ -17,18 +19,16 @@ use `dbus-send`. For example:
 #
 # Toggle LED
 #
-dbus-send --system --type=method_call --dest=com.eladalfassa.LedProxy /Leds com.eladalfassa.LedProxy.Toggle
+dbus-send --system --type=method_call --dest=com.eladalfassa.LedProxy /com/eladalfassa/LedProxy com.eladalfassa.LedProxy.Toggle
 #
 # Turn LED off
 #
-dbus-send --system --type=method_call --dest=com.eladalfassa.LedProxy /Leds com.eladalfassa.LedProxy.TurnOff
+dbus-send --system --type=method_call --dest=com.eladalfassa.LedProxy /com/eladalfassa/LedProxy com.eladalfassa.LedProxy.TurnOff
 #
 # Turn LED on
 #
-dbus-send --system --type=method_call --dest=com.eladalfassa.LedProxy /Leds com.eladalfassa.LedProxy.TurnOn
+dbus-send --system --type=method_call --dest=com.eladalfassa.LedProxy /com/eladalfassa/LedProxy com.eladalfassa.LedProxy.TurnOn
 ```
-
-dependencies: `sudo dnf intall python3-dbus python3-gobject`
 
 Why?
 ----
@@ -47,5 +47,3 @@ This is a very basic implementation. It should be improved to support more than
 one LED (right now it'll just use the first scroll-lock LED it finds, which might not actually exist, and may not be the one you want).
 
 It would also make sense to use a proper build system (like meson), and not just a basic shell script.
-
-Also, re-write to use GDbus w/ pygobject, because apparently dbus-python is deperecated.
--- ledproxy.py
+++ ledproxy.py
@@ -16,17 +16,25 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import dbus
-import dbus.service
+import pydbus
 import os
 import os.path
-from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 
 
-class LedService(dbus.service.Object):
-    def __init__(self, conn=None, object_path=None, bus_name=None):
-        super().__init__(conn, object_path, bus_name)
+class LedService(object):
+    """
+    <node>
+        <interface name="com.eladalfassa.LedProxy">
+            <method name="Toggle">
+            </method>
+            <method name="TurnOn">
+            </method>
+            <method name="TurnOff">
+            </method>
+        </interface>
+    </node>
+    """
 
     def get_led_path(self):
         """Get path to the first available scroll-lock LED"""
@@ -35,7 +43,6 @@ class LedService(dbus.service.Object):
                 return f"/sys/class/leds/{led}"
         return False
 
-    @dbus.service.method("com.eladalfassa.LedProxy")
     def Toggle(self):
         """Toggle LED"""
         path = self.get_led_path()
@@ -44,14 +51,12 @@ class LedService(dbus.service.Object):
             f.seek(0)
             f.write(str(int(not current_state)))
 
-    @dbus.service.method("com.eladalfassa.LedProxy")
     def TurnOn(self):
         """Turn LED on"""
         path = self.get_led_path()
         with open(os.path.join(path, "brightness"), "r+") as f:
             f.write('1')
 
-    @dbus.service.method("com.eladalfassa.LedProxy")
     def TurnOff(self):
         """Turn LED off"""
         path = self.get_led_path()
@@ -60,11 +65,9 @@ class LedService(dbus.service.Object):
 
 
 def main():
-    DBusGMainLoop(set_as_default=True)
 
-    bus = dbus.SystemBus()
-    name = dbus.service.BusName("com.eladalfassa.LedProxy", bus)
-    service = LedService(bus, "/Leds")
+    bus = pydbus.SystemBus()
+    bus.publish("com.eladalfassa.LedProxy", LedService())
 
     mainloop = GLib.MainLoop()
     mainloop.run()
